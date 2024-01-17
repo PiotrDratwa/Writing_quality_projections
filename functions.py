d73e3f1a3e57126094f243ec2094cef8e49ba3c5
PiotrDratwa <151626968+PiotrDratwa@users.noreply.github.com>
2024-01-17 19:55:37 +0100

    Delete functions.py

--- functions.py
+++
@@ -1,6 +0,0 @@
-def group_by_count(df, X, column, value, name):
-    temp = df[['id', column]]
-    temp = temp[temp[column] == value] 
-    X = X.join(temp.groupby(['id']).count())
-    X.rename(columns = {column:name}, inplace = True)
-    return X
